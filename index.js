b0e0239a87642ec511e21b9d2075b6d5a101659b
Mathieu Dutour <mathieu@dutour.me>
2017-01-15 20:53:50 +0000

    check if null

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+function isPresent (data) {
+  return typeof data !== 'undefined' && data !== null
+}
+
 module.exports = {
   getUserPreferences: function (pluginName, defaultPrefs) {
     var prefs = {}
     var store = NSUserDefaults.standardUserDefaults()
     Object.keys(defaultPrefs).forEach(function (k) {
       if (typeof defaultPrefs[k] === 'boolean') {
-        prefs[k] = typeof store.boolForKey(pluginName + k) !== 'undefined' ? Boolean(store.boolForKey(pluginName + k)) : defaultPrefs[k]
+        prefs[k] = isPresent(store.boolForKey(pluginName + k)) ? Boolean(store.boolForKey(pluginName + k)) : defaultPrefs[k]
       } else if (typeof defaultPrefs[k] === 'number') {
-        prefs[k] = typeof store.doubleForKey(pluginName + k) !== 'undefined' ? store.boolForKey(pluginName + k) : defaultPrefs[k]
+        prefs[k] = isPresent(store.doubleForKey(pluginName + k)) ? store.boolForKey(pluginName + k) : defaultPrefs[k]
       } else if (typeof defaultPrefs[k] === 'string') {
         prefs[k] = ('' + store.stringForKey(pluginName + k)) || defaultPrefs[k]
       } else if (Array.isArray(defaultPrefs[k])) {
