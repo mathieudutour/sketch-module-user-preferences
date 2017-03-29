96c05e1c5c3adc0c368905dfddd1927f169cc55c
Mathieu Dutour <mathieu@dutour.me>
2017-03-29 12:18:20 +0100

    use custom plist instead of using the sketch one

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ function isPresent (data) {
 module.exports = {
   getUserPreferences: function (pluginName, defaultPrefs) {
     var prefs = {}
-    var store = NSUserDefaults.standardUserDefaults()
+    var store = NSUserDefaults.alloc().initWithSuiteName('plugin.sketch.' + pluginName)
     Object.keys(defaultPrefs).forEach(function (k) {
       if (typeof defaultPrefs[k] === 'boolean') {
-        prefs[k] = isPresent(store.boolForKey(pluginName + k)) ? Boolean(store.boolForKey(pluginName + k)) : defaultPrefs[k]
+        prefs[k] = isPresent(store.boolForKey(k)) ? Boolean(store.boolForKey(k)) : defaultPrefs[k]
       } else if (typeof defaultPrefs[k] === 'number') {
-        prefs[k] = isPresent(store.doubleForKey(pluginName + k)) ? store.boolForKey(pluginName + k) : defaultPrefs[k]
+        prefs[k] = isPresent(store.doubleForKey(k)) ? store.boolForKey(k) : defaultPrefs[k]
       } else if (typeof defaultPrefs[k] === 'string') {
-        prefs[k] = ('' + store.stringForKey(pluginName + k)) || defaultPrefs[k]
+        prefs[k] = ('' + store.stringForKey(k)) || defaultPrefs[k]
       } else if (Array.isArray(defaultPrefs[k])) {
-        prefs[k] = store.arrayForKey(pluginName + k) || defaultPrefs[k]
+        prefs[k] = store.arrayForKey(k) || defaultPrefs[k]
       } else {
-        prefs[k] = store.dictionaryForKey(pluginName + k) || defaultPrefs[k]
+        prefs[k] = store.dictionaryForKey(k) || defaultPrefs[k]
       }
     })
     return prefs
   },
   setUserPreferences: function (pluginName, prefs) {
-    var store = NSUserDefaults.standardUserDefaults()
+    var store = NSUserDefaults.alloc().initWithSuiteName('plugin.sketch.' + pluginName)
     Object.keys(prefs).forEach(function (k) {
       if (typeof prefs[k] === 'boolean') {
-        store.setBool_forKey(prefs[k], pluginName + k)
+        store.setBool_forKey(prefs[k], k)
       } else if (typeof prefs[k] === 'number') {
-        store.setDouble_forKey(prefs[k], pluginName + k)
+        store.setDouble_forKey(prefs[k], k)
       } else {
-        store.setObject_forKey(prefs[k], pluginName + k)
+        store.setObject_forKey(prefs[k], k)
       }
     })
     store.synchronize()
